a65f6808a33a26c00db622321c7222b9a41291e6
rferdarko <65411244+rferdarko@users.noreply.github.com>
2021-10-29 02:50:22 +0000

    Add files via upload

---
+++ LearningPython/Hello.py
@@ -0,0 +1,5 @@
+print('Hello')
+a, b, c = 2, 3, 4
+x = a + b + c
+print(x)
+print(x * x)
---
+++ LearningPython/PrintName.py
@@ -0,0 +1,16 @@
+# PrintName
+# Requests user input then prints the input to the screen
+
+name = input('What is your name? ')  # Requests for information
+streetAddress = input('What is your street address? ')
+city = input('What city do you live in? ')
+state = input('What state do you live in? ')
+zipcode = input('What is your Zip Code? ')
+print()
+print('You entered ' + name) # prints user input
+print('Street: ' + streetAddress)
+print('City: ' + city)
+print('State: ' + state)
+print('Zip Code: ' + zipcode)
+
+
