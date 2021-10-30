7421599502ab0f37bdeca26319ff8f357d8a6de5
Robert Ferdarko <rferdarko@gmail.com>
2021-10-30 19:34:44 -0400

    First Commit

--- Samples/sample1.py
+++ Samples/sample1.py
@@ -1,1 +1,10 @@
-print('Sample 1 Test')
+print('Sample 1 Test\n')
+
+print('Python Fundamental Data Types: ')
+print('int, ' +'float, ' + 'bool, ' + 'str, ' + 'list, ' + 'tuple, ' + 'set, ' + 'dict\n')
+
+print('Classes: ')
+
+print('Specialized Data Types: ')
+
+print('None: \n')
