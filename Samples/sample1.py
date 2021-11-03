6e8174cb5e394849aff8232876d241c717ef6b77
Robert Ferdarko <rferdarko@gmail.com>
2021-11-03 00:01:41 -0400

    Third Commit

--- Samples/sample1.py
+++ Samples/sample1.py
@@ -27,3 +27,51 @@ print()
 # Multithreading
 print('Number of threads running: ' + str(threading.active_count()))
 print('Thread Name: ' + str(threading.current_thread()))
+print()
+
+# Binary Numbers - gives a binary representation of numbers.
+x = bin(5)
+print('Binary number for 5 is: ' + x)
+
+# We can also achieve the same result by using
+print(bin(5))
+# Or
+print('The binary number of 5 is: ' + bin(5))
+
+# To convert binary to integer, we use base 2
+print('The integer value of binary number 0b101 is: ' + str(int('0b101', 2)))
+
+
+# Augmented Assignment Operator
+# if we assign a value to an operator such as
+iq = 100
+print(iq)
+# we can change the value by using an assignment operator such as + or - , etc...
+iq += 10 # This will increase the value of iq by adding 10 to the assigned value
+print(iq)
+iq -= 20
+print(iq)
+print()
+
+# Strings
+a = str('Hi')
+print(a)
+
+# Long String
+# use 3 single quotes to start and end a long string
+long_string = '''
+Hi
+How 
+Are 
+you?
+'''
+print(long_string)
+
+first_name = 'Robert'
+last_name = 'Ferdarko'
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+
+
+
