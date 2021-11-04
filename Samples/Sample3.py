c1ea1175ba69f28a106a96b62e1be58d2b7ce84d
Robert Ferdarko <rferdarko@gmail.com>
2021-11-03 20:43:33 -0400

    First Commit

---
+++ Samples/Sample3.py
@@ -0,0 +1,11 @@
+# Matrix - multidimensional array
+my_matrix = [
+    [1,2,3,4,5,6,7,8,9,10],  # Row Index 0
+    [2,4,6,8,10,12,14,16,18,20],  # Row Index 1
+    [7,8,9,10,11,12,13,14,15,16]   # Row Index 2
+]
+print(my_matrix[0][2]) # prints top row, 3rd column index
+print(my_matrix[1][4]) # prints 2nd row, 5th column index
+print(my_matrix[2][9]) # prints 3rd row, 10th column index
+
+
