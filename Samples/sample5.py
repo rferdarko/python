442a7a19b7cc1c00a94acc036eb3dca8492f17ee
Robert Ferdarko <rferdarko@gmail.com>
2021-11-04 23:50:20 -0400

    Second Commit

--- Samples/sample5.py
+++ Samples/sample5.py
@@ -1,19 +1,21 @@
 # Dictionary   dict()
 # A way to organize data in Python through 'key' : 'value' pairs
 
-dictionary = {
-    'a': [1,2,3,4,5,6],
-    'b': 2,
-    'c': 3,
-    'd': 4
+dictionary = {            # start of dictionary
+    'a': [1,2,3,4,5,6],   # key:value array
+    'b': 2,               # key:value
+    'c': 3,               # key:value
+    'd': 4                # key:value
 }
 print('Dictionary')
 print(dictionary) # Prints entire dictionary
 print(dictionary['b'])  # Prints the value for dictionary key 'b'
 print(dictionary['a'][4])
+print()
 
 
 # This option created multiple dictionaries contained withing a list
+# keep in mind a dictionary is not sorted, whereas a list is sorted
 mylist = [     # Start of list
     {  # dictionary 0
         'a': [99,88,77,66,55,44,33,22,11],   # key: value array
@@ -22,13 +24,15 @@ mylist = [     # Start of list
 },     # End of dictionary 0
 {      # dictionary 1
     'a': [15,16,17],     # key:value array
-    'b': 'z',            # key:value
-    'c': 'w'             #kay:value
+    'b': 'zoo',            # key:value
+    'c': 'what'             #kay:value
 }   # End of dictionary 1
 
 ]   # End of list
-
+print('mylist\n')
 print(mylist[1]['a'][2])   # Prints from list, dictionary '1' key 'a' value '2'
 print(mylist[0]['a'][3])   # Prints from list, dictionary '0' key 'a' value '3'
 print(mylist[1]['b'])   # Prints from list, dictionary '1' key 'b'
 print(mylist[0]['c'][4])   # Prints from list, dictionary '0' key 'c' value '4'
+
+
