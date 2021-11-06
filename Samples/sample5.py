3887f047c42f4ee3ad8f689179affc3906d9c3b6
Robert Ferdarko <rferdarko@gmail.com>
2021-11-05 21:25:44 -0400

    Third Commit

--- Samples/sample5.py
+++ Samples/sample5.py
@@ -18,9 +18,9 @@ print()
 # keep in mind a dictionary is not sorted, whereas a list is sorted
 mylist = [     # Start of list
     {  # dictionary 0
-        'a': [99,88,77,66,55,44,33,22,11],   # key: value list
+        'a': [99,88,77,66,55,44,33,22],   # key: value list
         'b': 'X',                            # key : value
-        'c': [21,22,23,24,25,26]             # key:value list
+        11 : [21,22,23,24,25,26]             # key:value list
 },     # End of dictionary 0
 {      # dictionary 1
     'a': [15,16,17],     # key:value list
@@ -31,8 +31,8 @@ mylist = [     # Start of list
 ]   # End of list
 print('mylist\n')
 print(mylist[1]['a'][2])   # Prints from list, dictionary '1' key 'a' value '2'
-print(mylist[0]['a'][3])   # Prints from list, dictionary '0' key 'a' value '3'
+print(mylist[0][11][3])   # Prints from list, dictionary '0' key 'a' value '3'
 print(mylist[1]['b'])   # Prints from list, dictionary '1' key 'b'
-print(mylist[0]['c'][4])   # Prints from list, dictionary '0' key 'c' value '4'
+print(mylist[1]['c'][1])   # Prints from list, dictionary '0' key 'c' value '4'
 
 
