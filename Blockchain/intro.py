c27447565ce87436ff4d65ea7641d7cdc522514b
Robert Ferdarko <rferdarko@gmail.com>
2021-12-08 22:36:46 -0500

    Third Commit

--- Blockchain/intro.py
+++ Blockchain/intro.py
@@ -2,7 +2,7 @@
 
 my_name = input('Please enter your name: ')  # Requests user input and assigns it to my_name variable
 file = open('name.txt' , mode='w') # creates a name.txt file in write mode
-file.write(my_name) # writes mt_name variable content to the name.txt file
+file.write(my_name) # writes my_name variable content to the name.txt file
 file.close() # closes the file
 file = open('name.txt' , mode='r') # opens name.txt file in read mode
 content = file.read() # reads file and assigns data to content variable
